6b97dfcc46ff74f09456068e79c1d92a544b59b5
kitze012 <sskitzerow@gmail.com>
2023-04-18 23:18:04 -0500

    Adding hash tables and c code for linked lists

--- app.py
+++ app.py
@@ -52,6 +52,11 @@ def useful_tools():
 def page_not_found(error):
     return render_template('404.html', nav_id="home-page"), 404
 
+# Custom page not found
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('404.html', nav_id="home-page"), 404
+
 # ====================================================================
 # Subpages of Data Types [REMOVE]
 # ====================================================================
