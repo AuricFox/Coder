9911b239e183bd678ecbe4adfbe14caa710bd54d
kitze012 <sskitzerow@gmail.com>
2024-03-12 23:39:41 -0500

    Updating data structure routing to remove pagename

--- app/data_structures/routes.py
+++ app/data_structures/routes.py
@@ -11,19 +11,22 @@ def index():
 # Subpages of Data Structures
 # ====================================================================
 
-@bp.route('/<path:pagename>')
-def datastructure_route(pagename):
-    if pagename == 'Main':
-        return render_template('data_structures.html', nav_id="struct-page")
-    elif pagename == 'arrays':
-        return render_template('data_structures/arrays.html', nav_id="struct-page")
-    elif pagename == 'linked_lists':
-        return render_template('data_structures/linked_lists.html', nav_id="struct-page")
-    elif pagename == 'queues':
-        return render_template('data_structures/queues.html', nav_id="struct-page")
-    elif pagename == 'stacks':
-        return render_template('data_structures/stacks.html', nav_id="struct-page")
-    elif pagename == 'hash_tables':
-        return render_template('data_structures/hash_tables.html', nav_id="struct-page")
-    else:
-        return render_template('404.html', nav_id="struct-page")
+@bp.route('/arrays')
+def arrays():
+    return render_template('data_structures/arrays.html', nav_id="struct-page")
+
+@bp.route('/linked_lists')
+def linked_lists():
+    return render_template('data_structures/linked_lists.html', nav_id="struct-page")
+
+@bp.route('/queues')
+def queues():
+    return render_template('data_structures/queues.html', nav_id="struct-page")
+
+@bp.route('/stacks')
+def stacks():
+    return render_template('data_structures/stacks.html', nav_id="struct-page")
+
+@bp.route('/hash_tables')
+def hash_tables():
+    return render_template('data_structures/hash_tables.html', nav_id="struct-page")
