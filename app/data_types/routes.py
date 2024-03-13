3f51a7bf2ebbbc5f3b2f179811a5b8af2e190ad9
kitze012 <sskitzerow@gmail.com>
2024-03-12 23:48:01 -0500

    Updating data type routing to remove pagename

--- app/data_types/routes.py
+++ app/data_types/routes.py
@@ -11,23 +11,30 @@ def index():
 # Subpages of Data Types
 # ====================================================================
 
-@bp.route('/<path:pagename>')
-def datatypes_route(pagename):
-    if pagename == 'Main':
-        return render_template('data_types.html', nav_id="data-page")
-    elif pagename == 'integers':
-        return render_template('data_types/integers.html', nav_id="data-page")
-    elif pagename == 'floats':
-        return render_template('data_types/floats.html', nav_id="data-page")
-    elif pagename == 'dates':
-        return render_template('data_types/dates.html', nav_id="data-page")
-    elif pagename == 'characters':
-        return render_template('data_types/characters.html', nav_id="data-page")
-    elif pagename == 'strings':
-        return render_template('data_types/strings.html', nav_id="data-page")
-    elif pagename == 'booleans':
-        return render_template('data_types/booleans.html', nav_id="data-page")
-    elif pagename == 'nulls':
-        return render_template('data_types/nulls.html', nav_id="data-page")
-    else:
-        return render_template('404.html', nav_id="data-page")
+@bp.route('/integers')
+def integers():
+    return render_template('data_types/integers.html', nav_id="data-page")
+
+@bp.route('/floats')
+def floats():
+    return render_template('data_types/floats.html', nav_id="data-page")
+
+@bp.route('/dates')
+def dates():
+    return render_template('data_types/dates.html', nav_id="data-page")
+
+@bp.route('/characters')
+def characters():
+    return render_template('data_types/characters.html', nav_id="data-page")
+
+@bp.route('/strings')
+def strings():
+    return render_template('data_types/strings.html', nav_id="data-page")
+
+@bp.route('/booleans')
+def booleans():
+    return render_template('data_types/booleans.html', nav_id="data-page")
+
+@bp.route('/nulls')
+def nulls():
+    return render_template('data_types/nulls.html', nav_id="data-page")
