1ce206aa47e63eae2147c439709dc9faee3ec286
kitze012 <sskitzerow@gmail.com>
2024-03-13 20:52:26 -0500

    Reworking sub-nav elements

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,58 +1,21 @@
 // ===================================================================
 // Toggles display of sub navigation elements
 // ===================================================================
+// Display first level of dropdown elements
+$('.dropbtn').hover(function () {
+    // Hide all dropdown elements
+    $('.dropdown-element').hide();
+    // Get current highlighted element
+    const menu = $(this).next('.dropdown-element');
+    menu.show();
 
-var navDisplay = {
-    display: 'flex',
-    flexDirection: 'column'
-};
-
-$('#data-pages').click(() => {
-    $('.sub-nav').not($('#data-nav')).hide();
-
-    if($('#data-nav').is(':visible')) {$('#data-nav').hide();}
-    else {$('#data-nav').css(navDisplay).show();}
+    // Hide the dropdown when the mouse leaves the dropdown or the button
+    menu.on('mouseleave', function () {
+        menu.hide();
+    });
 });
 
-// ----------------------------------------------------------
-$('#struct-pages').click(() => {
-    $('.sub-nav').not($('#struct-nav')).hide();
-
-    if ($('#struct-nav').is(':visible')) { $('#struct-nav').hide(); }
-    else { $('#struct-nav').css(navDisplay).show(); }
-});
-
-// ----------------------------------------------------------
-$('#lang-pages').click(() => {
-    $('.sub-nav').not($('#lang-nav')).hide();
-
-    if($('#lang-nav').is(':visible')) {$('#lang-nav').hide();}
-    else {$('#lang-nav').css(navDisplay).show();}
-});
-
-// ----------------------------------------------------------
-$('#algo-pages').click(() => {
-    $('.sub-nav').not($('#algo-nav')).hide();
-
-    if($('#algo-nav').is(':visible')) {$('#algo-nav').hide();}
-    else {$('#algo-nav').css(navDisplay).show();}
-});
-
-// ----------------------------------------------------------
-$('#math-pages').click(() => {
-    $('.sub-nav').not($('#math-nav')).hide();
-
-    if($('#math-nav').is(':visible')) {$('#math-nav').hide();}
-    else {$('#math-nav').css(navDisplay).show();}
-});
-
-// ----------------------------------------------------------
-$('#tools-pages').click(() => {
-    $('.sub-nav').not($('#tools-nav')).hide();
-
-    if($('#tools-nav').is(':visible')) {$('#tools-nav').hide();}
-    else {$('#tools-nav').css(navDisplay).show();}
-});
+// Display second level of dropdown elements
 
 // ===================================================================
 // Toggles display for different coding languages [Data Types]
